d1baa16e4b4f70d4232e54e60d5749c5f51ba061
rbsarde <ardeschirc@gmail.com>
2025-02-14 12:45:32 +0100

    About and Contact Section fully added

--- src/components/ToolBoxItems.tsx
+++ src/components/ToolBoxItems.tsx
@@ -1,23 +1,38 @@
-import { TechIcon } from "./TechIcon"
+import { twMerge } from "tailwind-merge";
+import { TechIcon } from "./TechIcon";
 
-export const ToolBoxItems = ({ items }: {
-    items: {
-        title: string;
-        iconType: React.ElementType;
-    }[]
+export const ToolBoxItems = ({
+  items,
+  classname,
+  itemsWrapperClassName
+}: {
+  items: {
+    title: string;
+    iconType: React.ElementType;
+  }[];
+  classname?: string;
+  itemsWrapperClassName?: string;
 }) => {
-    return (
-        <div className="flex [mask-image:linear-gradient(to_righ,transparent,black_10%,black_90%,transparent)]">
-            <div className="flex flex-none">
-              {items.map((item) => (
-                <div key={item.title} className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
-                  <TechIcon component={item.iconType} />
-                  <span className="font-semibold ">{item.title}</span>
-                </div>
-              ))}
-            </div>
-            </div>
-    )
-}
+  return (
+    <div
+      className={twMerge(
+        "flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]",
+        classname
+      )}
+    >
+      <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6", itemsWrapperClassName)}>
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+          >
+            <TechIcon component={item.iconType} />
+            <span className="font-semibold ">{item.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 //
